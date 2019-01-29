3a5a42c936457f3ffef73286bc36e6a8c17da7b3
KM <sodp2@naver.com>
2019-01-29 22:25:18 +0900

    15552: solve 빠른 A+B (coding)

---
+++ BaekJoon/KM/빠른 A+B_15552.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main(void)
+{
+	int T;
+	int A, B;
+
+	scanf("%d", &T);
+
+	for (auto i = 0; i < T; ++i)
+	{
+		scanf("%d %d", &A, &B);
+
+		printf("%d\n", A + B);
+	}
+
+	return 0;
+}
