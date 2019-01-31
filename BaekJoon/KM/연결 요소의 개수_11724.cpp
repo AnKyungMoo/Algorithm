74d1d5ed0ac5aeb897c1e5336f0e796cbeca304c
KM <sodp2@naver.com>
2019-02-01 00:37:25 +0900

    11724: solve 연결 요소의 개수 (union-find)

---
+++ BaekJoon/KM/연결 요소의 개수_11724.cpp
@@ -0,0 +1,51 @@
+﻿#include <iostream>
+#include <set>
+
+using namespace std;
+
+int parent[1001];
+
+int find(int a)
+{
+	if (parent[a] == a)
+		return a;
+
+	return parent[a] = find(parent[a]);
+}
+
+void union_(int a, int b)
+{
+	int a_root = find(a);
+	int b_root = find(b);
+
+	parent[a_root] = parent[b_root];
+}
+
+int main(void)
+{
+	int N, M;
+	int a, b;
+	set<int> s;
+
+	scanf("%d %d", &N, &M);
+
+	for (auto i = 1; i <= N; ++i)
+	{
+		parent[i] = i;
+	}
+
+	for (auto i = 1; i <= M; ++i)
+	{
+		scanf("%d %d", &a, &b);
+		union_(a, b);
+	}
+
+	for (auto i = 1; i <= N; ++i)
+	{
+		s.insert(find(i));
+	}
+
+	printf("%d", s.size());
+
+	return 0;
+}
