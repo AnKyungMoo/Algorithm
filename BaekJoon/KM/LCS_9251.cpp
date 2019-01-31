0a98fd735ce5393b28ad0a42199a7ac8bd5ff158
KM <sodp2@naver.com>
2019-02-01 01:30:48 +0900

    9251: solve LCS (LCS)

---
+++ BaekJoon/KM/LCS_9251.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+#include <string>
+#include <algorithm>
+
+using namespace std;
+
+int LCS[1001][1001];
+
+int main(void)
+{
+	string s1, s2;
+
+	cin >> s1;
+	cin >> s2;
+
+	for (auto i = 0; i < s1.length(); ++i)
+	{
+		for (auto j = 0; j < s2.length(); ++j)
+		{
+			if (s1[i] == s2[j])
+			{
+				LCS[i + 1][j + 1] = LCS[i][j] + 1;
+			}
+			else
+			{
+				LCS[i + 1][j + 1] = max(LCS[i+1][j], LCS[i][j + 1]);
+			}
+		}
+	}
+
+	printf("%d", LCS[s1.length()][s2.length()]);
+
+	return 0;
+}
