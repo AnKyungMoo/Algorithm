fbf063653beb40388ba662ec2ef2730394ddefec
KM <sodp2@naver.com>
2019-02-25 13:03:32 +0900

    11650: solve 좌표 정렬하기 (sort)

---
+++ BaekJoon/좌표 정렬하기_11650.java
@@ -0,0 +1,51 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+
+class Point implements Comparable<Point>{
+    int x, y;
+
+    Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    @Override
+    public int compareTo(Point p) {
+        if (this.x > p.x) {
+            return 1;
+        } else if (this.x == p.x) {
+            if (this.y > p.y) {
+                return 1;
+            }
+        }
+        return -1;
+    }
+}
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        ArrayList<Point> arrayList = new ArrayList<>();
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < N; ++i) {
+            String insert = br.readLine();
+            String[] s = insert.split(" ");
+            int x = Integer.parseInt(s[0]);
+            int y = Integer.parseInt(s[1]);
+
+            arrayList.add(new Point(x, y));
+        }
+
+        Collections.sort(arrayList);
+
+        for (Point point : arrayList) {
+            System.out.println(point.x + " " + point.y);
+        }
+    }
+}
